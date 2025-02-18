48e4afab11cc87a214a44b7ed29744719dbaf6cc
Frazor Sharp <daocamberskies@googlemail.com>
2025-02-18 15:17:40 +0000

    render.rs cleanup

--- src/application/render.rs
+++ src/application/render.rs
@@ -1,6 +1,6 @@
-use super::{ecs::components::*, App};
 use sfml::graphics::{Color, RectangleShape, RenderTarget};
 use specs::{Join, WorldExt};
+use super::{ecs::components::*, App};
 
 // values used are between 0 and 255
 const BACKGROUND_COLOR: Color = Color {
@@ -11,21 +11,16 @@ const BACKGROUND_COLOR: Color = Color {
 };
 
 pub fn render_all(main_app: &mut App) {
-    // create a list of all things to be redered
-
     // clear the back buffer
     main_app.window.clear(BACKGROUND_COLOR);
 
     // draw all current objects to the back buffer
-    // main_app.window.draw( Object to draw );
     draw_rectangles(main_app);
 
     // swap the completed back buffer to the front
     main_app.window.display();
 }
 
-// TODO: why are we constructing the rectangle here ???
-//  : can this be done when we create the entity ?
 fn draw_rectangles(app: &mut App) {
     // Gather the data
     let rectangles = app.world.read_storage::<CRectangle>();
